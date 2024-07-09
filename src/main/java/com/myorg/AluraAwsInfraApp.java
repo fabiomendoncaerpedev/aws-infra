8fd46aaca98a23d9e4c103eb97682317a659793f
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-07-09 11:25:27 -0300

    feat: creating rds database service

--- src/main/java/com/myorg/AluraAwsInfraApp.java
+++ src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -13,9 +13,11 @@ public class AluraAwsInfraApp {
         AluraVPCStack vpcStack = new AluraVPCStack(app, "VPC");
         AluraClusterStack clusterStack = new AluraClusterStack(app, "Cluster", vpcStack.getVpc());
         AluraServiceStack serviceStack = new AluraServiceStack(app, "Service", vpcStack.getVpc(), clusterStack.getCluster());
+        AluraRDSStack RDSStack = new AluraRDSStack(app, "RDS", vpcStack.getVpc());
 
         clusterStack.addDependency(vpcStack);
         serviceStack.addDependency(clusterStack);
+        RDSStack.addDependency(vpcStack);
 
         app.synth();
     }
